9e340bfda71b98a2dd9ff97036b2265f1800a90d
jhosuapp <jhosuapvll@gmail.com>
2024-04-24 20:48:17 -0500

    feat: ui hero section and containers

--- src/tsx/components/HeroSection.tsx
+++ src/tsx/components/HeroSection.tsx
@@ -3,14 +3,19 @@ import imageHero from '../../assets/png/rick-and-morty.png';
 import { HTMLAttributes } from 'react';
 //Components
 import { Container } from './Container';
+//Icon
+import bg from '../../assets/svg/bg.svg';
 
 type Props = HTMLAttributes<HTMLImageElement>;
 
 const HeroSection = ( { ...PropsPicture }: Props):JSX.Element=>{
     return (
-        <Container className='container'>
+        <Container className='container hero-section container--relative container--center'>
             <picture { ...PropsPicture }>
-                <img  src={ imageHero } />
+                <img  src={ imageHero } alt="hero image"/>
+            </picture>
+            <picture { ...PropsPicture }>
+                <img src={ bg } alt="background hero"/>
             </picture>
         </Container>
     )
--- src/tsx/components/home/Home.tsx
+++ src/tsx/components/home/Home.tsx
@@ -1,10 +1,16 @@
-
+//Components
 import { HeroSection } from "../../components/HeroSection";;
+import { Container } from "../Container";
 
 const Home = ():JSX.Element=>{
     return (
         <>
-            <HeroSection className="hero-section" />
+            <HeroSection className="hero-item" />
+            <Container className="container--bg">
+                <Container className="container">
+                    <p>hola</p>
+                </Container>
+            </Container>
         </>
     )
 }
