a3caf37c52cb41f4e374b97e604700532614c16d
jhosuapp <jhosuapvll@gmail.com>
2024-04-29 14:57:42 -0500

    feat: ui for cards

--- src/tsx/components/home/Home.tsx
+++ src/tsx/components/home/Home.tsx
@@ -1,14 +1,54 @@
 //Components
 import { HeroSection } from '../global/HeroSection';
 import { Container } from '../global/Container';
+import { Link } from 'react-router-dom';
 
 const Home = ():JSX.Element=>{
     return (
         <>
             <HeroSection className="hero-item" />
             <Container className="container--bg">
-                <Container className="container">
-                    <p>hola</p>
+                <Container className="container list-characters">
+                    {/* Item */}
+                    <Link to="/character/231312">
+                        <picture>
+                            <img src="https://rickandmortyapi.com/api/character/avatar/132.jpeg" alt="image" />
+                        </picture>
+                        <ul>
+                            <li>
+                                <h2>Gar's Mytholog</h2>
+                                <h5>dead - Mythological Creature</h5>
+                            </li>
+                            <li>
+                                <p>Last known location:</p>
+                                <p>Nuptia 4</p>
+                            </li>
+                            <li>
+                                <p>First seen in:</p>
+                                <p>Big trouble in little Sanchez</p>
+                            </li>
+                        </ul>
+                    </Link>
+                    <Link to="/character/231312">
+                        <picture>
+                            <img src="https://rickandmortyapi.com/api/character/avatar/132.jpeg" alt="image" />
+                        </picture>
+                        <ul>
+                            <li>
+                                <h2>Gar's Mytholog</h2>
+                                <h5>dead - Mythological Creature</h5>
+                            </li>
+                            <li>
+                                <p>Last known location:</p>
+                                <p>Nuptia 4</p>
+                            </li>
+                            <li>
+                                <p>First seen in:</p>
+                                <p>Big trouble in little Sanchez</p>
+                            </li>
+                        </ul>
+                    </Link>
+                    {/* Item */}
                 </Container>
             </Container>
         </>
